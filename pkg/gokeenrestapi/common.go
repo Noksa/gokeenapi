c39ee84246d33dbf3d7fb185f7aa5dc530d0187e
Alexandr Danilin <alexandr.danilin@ooma.com>
2026-05-21 21:02:11 +0300

    security: replace MD5 with SHA-256 for cache filename, fix file permissions
    - Use sha256.Sum256(url+login) instead of md5.Sum(url+login+password)
      for cache filename derivation: removes password from hash input and
      replaces cryptographically broken MD5 with SHA-256
    - Fix Save() file permissions: 0700 -> 0600 (no execute bit on data file)
    - Fix initial cache file creation permissions: os.ModePerm -> 0600

--- pkg/gokeenrestapi/common.go
+++ pkg/gokeenrestapi/common.go
@@ -55,7 +55,7 @@ func (f *keeneticCacheFile) Save() error {
 	if err != nil {
 		return err
 	}
-	err = os.WriteFile(f.path, b, 0700)
+	err = os.WriteFile(f.path, b, 0600)
 	return err
 }
 
@@ -87,7 +87,7 @@ func (c *keeneticCommon) getKeeneticCacheFile() (keeneticCacheFile, error) {
 		}
 		cleanedOldCacheFiles = true
 	}
-	bHash := md5.Sum(fmt.Appendf(nil, "%v-%v-%v", config.Cfg.Keenetic.URL, config.Cfg.Keenetic.Login, config.Cfg.Keenetic.Password))
+	bHash := sha256.Sum256(fmt.Appendf(nil, "%v-%v", config.Cfg.Keenetic.URL, config.Cfg.Keenetic.Login))
 	hashString := fmt.Sprintf("%x", bHash)
 	keeeticFile := path.Join(gokeenDir, fmt.Sprintf("%v.json", hashString))
 	_, statErr := os.Stat(keeeticFile)
@@ -95,7 +95,7 @@ func (c *keeneticCommon) getKeeneticCacheFile() (keeneticCacheFile, error) {
 		if !errors.Is(statErr, os.ErrNotExist) {
 			return keeneticCacheFile{}, statErr
 		}
-		err = os.WriteFile(keeeticFile, []byte("{}"), os.ModePerm)
+		err = os.WriteFile(keeeticFile, []byte("{}"), 0600)
 		if err != nil {
 			return keeneticCacheFile{}, err
 		}
